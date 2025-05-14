6f040c0f67f6fe4e74447e2500381c42a3cd67c9
Pumelela-Banca <pumelelabanca@gmail.com>
2025-05-14 23:08:47 +0200

    added template files

---
+++ MenuesBuild/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MenuesBuild
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ MenuesBuild/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace MenuesBuild
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected void FileExit_Click(object sender, RoutedEventArgs args)
+        {
+            // close window
+            this.Close();
+        }
+
+        protected void ToolsSpellingHints_Click(object sender, RoutedEventArgs args)
+        {
+        }
+        protected void MouseEnterExitArea(object sender, RoutedEventArgs args)
+        {
+        }
+        protected void MouseEnterToolsHintsArea(object sender, RoutedEventArgs args)
+        {
+        }
+        protected void MouseLeaveArea(object sender, RoutedEventArgs args)
+        {
+        }
+    }
+}
